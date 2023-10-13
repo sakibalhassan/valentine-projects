7b44c074b204e92a0091de18735ddda33771a043
Sakib Alhassan <sakibalhassan283@gmail.comgit config --global user.name>
2023-10-14 00:49:18 +0600

    added valantine all file

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$('.main-slide').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '<i class="fa-solid fa-chevron-left prev"></i>',
+    nextArrow: '<i class="fa-solid fa-chevron-right next"></i>',
+});
